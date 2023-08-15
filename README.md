8e574906c4838777bd50568073c68c0595e48d66
majabukowska <majabukowska8@gmail.com>
2023-08-15 18:10:33 +0200

    sorting + fizzbuz

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# algorithms
-Algorithms
+# FizzBuzz and Sorting Algorithms
+This repository contains Python code for implementing the FizzBuzz problem and several sorting algorithms: Bubble Sort, Quick Sort, Insertion Sort, Counting Sort, and Binary Search.
+
+### FizzBuzz
+The FizzBuzz problem is solved using the division function. Given a number, the function checks if it's divisible by 3, 5, or both, and returns "Fizz", "Buzz", "FizzBuzz", or the number itself accordingly. The create_list function generates a list of numbers from 1 to 100 and applies the division function to each number, resulting in a list of FizzBuzz outputs.
+
+## Sorting Algorithms
+
+### Bubble Sort
+The bubble function implements the Bubble Sort algorithm. It iterates through the input array and compares adjacent elements, swapping them if they are in the wrong order. This process is repeated until the entire array is sorted.
+
+### Quick Sort
+The quick_sort function demonstrates the Quick Sort algorithm. It selects a pivot element and divides the array into two sub-arrays - one containing elements smaller than the pivot and the other containing elements greater than the pivot. The function recursively sorts these sub-arrays and concatenates them around the pivot.
+
+### Insertion Sort
+The insertion_sort function showcases the Insertion Sort algorithm. It iterates through the array, comparing each element with the elements before it and moving them one position ahead if necessary to place the element in the correct sorted position.
+
+### Counting Sort
+The counting_sort function implements the Counting Sort algorithm, which is suitable for sorting integers within a specified range. It uses auxiliary arrays to count the occurrences of each element and then reassembles the sorted array.
+
+### Binary Search
+The binary_search function demonstrates the Binary Search algorithm. Given a sorted array and an item to search for, it repeatedly divides the search interval in half until the item is found or the interval is empty.
---
+++ algorithms.py
@@ -0,0 +1,103 @@
+# FIZZBUZZ
+def division(number):
+    if number % 3 == 0:
+        if number % 5 == 0:
+            return "FizzBuzz"
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    return number
+
+
+def create_list():
+    numbers = []
+    for i in range(1, 101):
+        numbers.append(i)
+    return numbers
+
+
+if __name__ == "__main__":
+    numbers = create_list()
+    finished_numbers = []
+    for i in numbers:
+        finished_numbers.append(division(i))
+    print(finished_numbers)
+
+# BUBBLE SORT
+
+def bubble(arr):
+    for i in range(len(arr)):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+# QUICK SORT
+
+def quick_sort(arr):
+    length = len(arr)
+    smaller_numbers = []
+    greater_numbers = []
+
+    if length <= 1:
+        return arr
+    else:
+        pivot = arr.pop()
+    for i in arr:
+        if i > pivot:
+            greater_numbers.append(i)
+        else:
+            smaller_numbers.append(i)
+    return quick_sort(smaller_numbers) + [pivot] + quick_sort(greater_numbers)
+
+
+# INSERTION SORT
+
+def insertion_sort(arr):
+    length = len(arr)
+
+    for i in range(1, length):
+        while arr[i - 1] > arr[i] and i > 0:
+            arr[i - 1], arr[i] = arr[i], arr[i - 1]
+            i -= 1
+
+    return arr
+
+# COUNTING SORT
+
+def counting_sort(arr):
+    arr_len = len(arr)
+    output = [0] * arr_len
+    arr_count = [0] * 10
+
+    for i in range(0, arr_len):
+        arr_count[arr[i]] += 1
+
+    for j in range(1, 10):
+        arr_count[j] += arr_count[j - 1]
+
+    a = arr_len - 1
+    while a >= 0:
+        output[arr_count[arr[a]] - 1] = arr[a]
+        arr_count[arr[a]] -= 1
+        a -= 1
+
+    for k in range(0, arr_len):
+        arr[k] = output[k]
+
+# BINARY SEARCH
+
+def binary_search(arr, item):
+    begin_index = 0
+    end_index = len(arr) - 1
+    while begin_index <= end_index:
+        midpoint = int(begin_index + (end_index - begin_index) / 2)
+        midpoint_value = arr[midpoint]
+        if item == midpoint_value:
+            return midpoint
+        elif item < midpoint_value:
+            end_index = midpoint - 1
+        else:
+            begin_index = midpoint + 1
+    return "Not found"
+
